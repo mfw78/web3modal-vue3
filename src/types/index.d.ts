2aa05ea85226dff09ec06d9405ead4c84c3f61eb
mfw78 <mfw78@chasingseed.com>
2021-09-14 18:37:51 +0000

    fix: 🐛 type export for component

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,5 +1,5 @@
 import { web3Modal } from "@/components/Web3Modal";
 import { App } from "vue";
 export declare const Web3ModalVuePlugin: { install: (app: App, ...options: any[]) => any };
-export declare const Web3ModalComponent: typeof web3Modal;
+export declare type Web3ModalComponent = typeof web3Modal;
 export default Web3ModalVuePlugin;
